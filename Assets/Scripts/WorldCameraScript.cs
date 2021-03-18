d945ffa2b031002e1f2aa4ac272892e56b5a6fa0
Ibai <Baglietto Arakistain>
2021-03-18 14:00:46 +0100

    World movement animations and camera movement
    World movement animations and camera movement

---
+++ Assets/Scripts/WorldCameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCameraScript : MonoBehaviour
+{
+    //The player
+    private GameObject player;
+
+    void Start()
+    {
+        //We find the player
+        player = GameObject.Find("PlayerWorld");
+        
+    }
+
+
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3.0f, player.transform.position.z - 8.0f);
+    }
+}
--- Assets/Scripts/WorldPlayerMovementScript.cs
+++ Assets/Scripts/WorldPlayerMovementScript.cs
@@ -1,17 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class WorldPlayerMovementScript : MonoBehaviour
 {
     //The direction the player is moving. 0-> not moving, 1-> right, 2-> left, 3 -> up, 4 -> down
     private float speedX;
     private float speedZ;
+    //A mask determining what is ground to the character
+    [SerializeField] private LayerMask whatIsGround;
+    //A position marking where to check if the player is grounded.
+    [SerializeField] private Transform groundCheck;
+
+    //Radius of the overlap circle to determine if grounded
+    const float groundedRadius = .2f;
+    //Whether or not the player is grounded.
+    private bool grounded;
+    //The animator
+    Animator animator;
+
+
+    //The on land event
+    [Header("Events")]
+    [Space]
+    public UnityEvent OnLandEvent;
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
+
+    private void Awake()
+    {
+        //We initialize the onLandEvent
+        if (OnLandEvent == null) OnLandEvent = new UnityEvent();
+    }
+
     void Start()
     {
         //We initialize the variables
         speedX = 0.0f;
         speedZ = 0.0f;
+        //We find the animator
+        animator = gameObject.GetComponent<Animator>();
     }
 
 
@@ -21,20 +50,62 @@ public class WorldPlayerMovementScript : MonoBehaviour
         if (Input.GetKey(KeyCode.UpArrow)) speedZ = 1.0f;
         else if (Input.GetKey(KeyCode.DownArrow)) speedZ = -1.0f;
         else speedZ = 0.0f;
-        if (Input.GetKey(KeyCode.RightArrow)) speedX = 1.0f;
-        else if (Input.GetKey(KeyCode.LeftArrow)) speedX = -1.0f;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            speedX = 1.0f;
+            animator.SetBool("RightLast", true);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            speedX = -1.0f;
+            animator.SetBool("RightLast", false);
+        }
         else speedX = 0.0f;
-        if(speedX!=0 || speedZ!=0) gameObject.GetComponent<Animator>().SetBool("Moving", true);
-        else gameObject.GetComponent<Animator>().SetBool("Moving", false);
-        gameObject.GetComponent<Animator>().SetFloat("SpeedZ", speedZ);
-        gameObject.GetComponent<Animator>().SetFloat("SpeedX", speedX);
+        if(speedX!=0 || speedZ!=0) animator.SetBool("Moving", true);
+        else animator.SetBool("Moving", false);
+        animator.SetFloat("SpeedZ", speedZ);
+        animator.SetFloat("SpeedX", speedX);
         //Make the player jump when space is pressed
-        if (Input.GetKeyDown(KeyCode.Space)) gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f,300.0f,0.0f));
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, 200.0f, 0.0f));
+            animator.SetBool("isJumping", true);
+        }
+        //We check if the player is falling
+        if (animator.GetBool("isJumping") && gameObject.GetComponent<Rigidbody>().velocity.y < -0.01f) animator.SetBool("isFalling", true);
     }
 
     private void FixedUpdate()
     {
+        bool wasGrounded = grounded;
+        grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        Collider[] colliders = Physics.OverlapSphere(groundCheck.position, groundedRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                grounded = true;
+                if (!wasGrounded)
+                    OnLandEvent.Invoke();
+            }
+        }
+
         //move the player on the direction we saved previously
         gameObject.GetComponent<Rigidbody>().velocity = new Vector3(speedX * 3, gameObject.GetComponent<Rigidbody>().velocity.y, speedZ * 3);
+    }
+
+    //When the player lands we uncheck the jumping and falling booleans
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+        animator.SetBool("isFalling", false);
+    }
+
+    //Function to know if the player is grounded
+    public bool IsGrounded()
+    {
+        return grounded;
     }
 }
