50dacccba6e29098ae37ecea10e61e108f9edcfb
Ibai <Baglietto Arakistain>
2021-08-13 13:58:59 +0200

    mesh simplifier
    mesh simplifier

---
+++ Assets/Scripts/MeshSimplifierScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSimplifierScript : MonoBehaviour
+{
+    public float quality;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var originalMesh = GetComponent<MeshFilter>().sharedMesh;
+        var meshSimplifier = new UnityMeshSimplifier.MeshSimplifier();
+        meshSimplifier.Initialize(originalMesh);
+        meshSimplifier.SimplifyMesh(quality);
+        var destMesh = meshSimplifier.ToMesh();
+        GetComponent<MeshFilter>().sharedMesh = destMesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
