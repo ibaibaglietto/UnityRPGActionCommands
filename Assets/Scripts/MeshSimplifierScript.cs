a8b5223aabd07d09e2937304c899548035fba2f5
Ibai <Baglietto Arakistain>
2021-08-17 14:03:44 +0200

    1-3 and lighting
    1-3 and lighting

--- Assets/Scripts/MeshSimplifierScript.cs
+++ Assets/Scripts/MeshSimplifierScript.cs
@@ -8,12 +8,12 @@ public class MeshSimplifierScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var originalMesh = GetComponent<MeshFilter>().sharedMesh;
-        var meshSimplifier = new UnityMeshSimplifier.MeshSimplifier();
-        meshSimplifier.Initialize(originalMesh);
-        meshSimplifier.SimplifyMesh(quality);
-        var destMesh = meshSimplifier.ToMesh();
-        GetComponent<MeshFilter>().sharedMesh = destMesh;
+        //var originalMesh = GetComponent<MeshFilter>().sharedMesh;
+        //var meshSimplifier = new UnityMeshSimplifier.MeshSimplifier();
+        //meshSimplifier.Initialize(originalMesh);
+        //meshSimplifier.SimplifyMesh(quality);
+        //var destMesh = meshSimplifier.ToMesh();
+        //GetComponent<MeshFilter>().sharedMesh = destMesh;
     }
 
     // Update is called once per frame
